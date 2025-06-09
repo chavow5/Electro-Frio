b52c1b70c33e90047b6d959da65f72c0ca5434a4
chavow5 <davidramirezw5@gmail.com>
2025-06-09 02:04:36 -0300

    form en home

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,10 @@ function App() {
       <Routes>
         {/* Agrega las rutas */}
         <Route path="/" element={<Home />} />
-        <Route path="/FormularioTrabajoTecnico" element={<FormularioTrabajoTecnico />} />
+        <Route path="/formulariotrabajotecnico" element={<FormularioTrabajoTecnico />} />
       </Routes>
+      {/* prueba formulario en pagina de inicio, despues cambiarlo a una pagina */}
+      <FormularioTrabajoTecnico/>
       <Footer />
     </Router>
   );
