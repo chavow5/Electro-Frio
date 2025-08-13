7ccbed52f833219e2179dd6eeabb5664994183c6
chavow5 <davidramirezw6@gmail.com>
2025-08-13 15:41:00 -0300

    componentes upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Equipos from './pages/Equipos';
-// import About from './pages/About';
+import About, { AboutUs } from './pages/AboutUs';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
-import FormularioTrabajoTecnico from './components/FormularioTrabajoTecnico.jsx';
-
+import FormularioTrabajoTecnico from './pages/FormularioTrabajoTecnico.jsx';
+import Servicios from './pages/Servicios.jsx';
+import GaleriaTrabajo from './pages/GaleriaTrabajo.jsx';
 
 function App() {
   return (
@@ -15,10 +16,17 @@ function App() {
         {/* Agrega las rutas */}
         <Route path="/" element={<Home />} />
         <Route path="/formulariotrabajotecnico" element={<FormularioTrabajoTecnico />} />
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/galeria" element={<GaleriaTrabajo />} />
+        <Route path="/sobrenosotros" element={<AboutUs />} />
+        
       </Routes>
-      {/* prueba formulario en pagina de inicio, despues cambiarlo a una pagina */}
+      {/* pruebas en pagina de inicio, despues cambiarlo a una ruta */}
       <FormularioTrabajoTecnico/>
       <Equipos/>
+      <Servicios/>
+      <GaleriaTrabajo/>
+      <AboutUs/>
       <Footer />
     </Router>
   );
